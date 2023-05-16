3bd356b62db35451372cda57f497442877bf2968
apple <apple.mangoooo97@gmail.com>
2023-05-16 20:36:13 +0900

    add abc301-a

---
+++ atcoder/abc301/a-overall_winner.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int N; cin >> N;
+    string S; cin >> S;
+
+    int T =0;
+    int A =0;
+    char last = 'T';
+
+    for(int i=0; i<N; i++){
+        char n = S[i];
+        if (n == 'T') {
+            T++;
+            last = 'A';
+            continue;
+        }
+        A++;
+        last = 'T';
+    }
+    if (T > A) {
+        cout << "T" << endl;
+        return 0;
+    }
+    if (T == A) {
+        cout << last << endl;
+        return 0;
+    }
+    cout << "A" << endl;
+
+    return 0;
+}
